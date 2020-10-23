e78884cd4333f144d2453198efcb4dc23ad732e2
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-10-24 00:41:20 +0900

    made create and show for credit card

--- app/controllers/credit_card_page_controller.rb
+++ app/controllers/credit_card_page_controller.rb
@@ -1,14 +1,23 @@
 class CreditCardPageController < ApplicationController
   def index
+    @user = current_user
   end
 
-  def create
+  def show
+    @card = current_user.credit_card
   end
 
   def new
+    @card = CreditCard.new
   end
 
   def create
+    @card = CreditCard.new(card_params)
+    @card.user_id = current_user.id
+
+    if @card.save
+      redirect_to credit_card_page_index_path
+    end
   end
 
   def edit
@@ -18,6 +27,12 @@ class CreditCardPageController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def card_params
+    params.require(:credit_card).permit(:number)
   end
 
 end
