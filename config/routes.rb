fb769e385568b9151546f803a33882efa5cddaf1
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-10-23 18:41:28 +0900

    homepage controller and wrote routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :homepage, only: [:index]
+  resources :credit_card_page
+  resources :comment_page
   #devise_for :users, controllers: {
     #sessions: 'users/sessions'
   #}
+  
+  root to: "homepage#index"
 end
