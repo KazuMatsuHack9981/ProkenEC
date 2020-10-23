6f958e9fa4dbb49a5741b1d51c73de0af8cf505d
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-10-23 18:30:02 +0900

    added method

--- app/controllers/credit_card_page_controller.rb
+++ app/controllers/credit_card_page_controller.rb
@@ -1,2 +1,23 @@
 class CreditCardPageController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
