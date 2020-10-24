7f8ea70a4e439ed7d4aefd39ebd7e82b2d277d4c
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-10-24 22:08:07 +0900

    complete !

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_one :credit_card
-  has_many :comment
+  has_one :credit_card, dependent: :destroy
+  has_many :comment, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
