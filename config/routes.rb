25d7598bf592a72ce41e5a84e980411fc2e83a68
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-10-24 22:48:08 +0900

    added admin user nil/true

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #devise_for :users, :controllers => {
+    #:registrations => 'users/registrations'
+  #}
 
   resources :homepage, only: [:index]
   resources :credit_card_page
   post "credit_card_page/confirmation"
   resources :comment_page
-  #devise_for :users, controllers: {
-    #sessions: 'users/sessions'
-  #}
   
   root to: "homepage#index"
 end
