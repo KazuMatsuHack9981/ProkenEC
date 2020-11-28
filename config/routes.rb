1fdecb38d45a52fe1c564f3c81f75de8e3499e4c
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-11-28 12:49:07 +0900

    couple minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :homepage, only: [:index]
   get "homepage/alluser"
-  resources :credit_card_page
+  resources :credit_card_page, param: :public_uid
   post "credit_card_page/confirmation"
   resources :comment_page
   
